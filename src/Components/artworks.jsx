373b023e243561ce3a97373f269de6bbc07fe9e3
Bhavana <aromalaravind0@gmail.com>
2025-04-21 23:24:15 +0530

    some changes

--- src/Components/artworks.jsx
+++ src/Components/artworks.jsx
@@ -6,19 +6,19 @@ export default function Artworks() {
      
      <div id="artworks" className="flex h-[80vh] overflow-x-scroll ">
 						<div className="flex mt-[10vh] gap-10 px-[45px] ">
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="./assets/work2.png"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="./assets/work3.png"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="./assets/work4.png"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="./assets/work5.png"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="./assets/work6.png"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="./assets/work7.png"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="./assets/work8.png"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="./assets/work9.jpg"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="./assets/work10.jpg"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="./assets/work11.jpeg"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="./assets/work1.png"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="./assets/work13.jpeg"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="./assets/work14.jpeg"></img>			
+            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work2.png"></img>
+            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work3.png"></img>
+            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work4.png"></img>
+            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work5.png"></img>
+            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work6.png"></img>
+            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work7.png"></img>
+            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work8.png"></img>
+            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work9.jpg"></img>
+            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work10.jpg"></img>
+            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work11.jpeg"></img>
+            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work1.png"></img>
+            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work13.jpeg"></img>
+            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work14.jpeg"></img>			
 						</div>
            
 					</div>
