3e1f1653f75c0e9d1f631b1e25987838a8309b51
aravinnndddd <aromalaravind0@gmail.com>
2025-04-26 09:05:43 +0530

    Update artworks.jsx

--- src/Components/artworks.jsx
+++ src/Components/artworks.jsx
@@ -1,32 +1,31 @@
-import work1 from "../assets/work2.png"
-export default function Artworks() {
 
+const artworks = [
+  'work2.png',
+  'work3.png',
+  'work4.png',
+  'work5.png',
+  'work6.png',
+  'work7.png',
+  'work8.png',
+  'work1.png',
+  'work10.jpg','work11.jpeg','work13.jpeg','work14.jpeg',
+];
+
+function Artworks() {
   return (
-    <>
-     
-     <div id="artworks" className="flex h-[80vh] overflow-x-scroll ">
-						<div className="flex mt-[10vh] gap-10 px-[45px] ">
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src={work1></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work3.png"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work4.png"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work5.png"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work6.png"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work7.png"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work8.png"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work9.jpg"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work10.jpg"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work11.jpeg"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work1.png"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work13.jpeg"></img>
-            <img className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]" src="../assets/work14.jpeg"></img>			
-						</div>
-           
-					</div>
-
-
-    </>
-  )
+							<div id="artworks" className="flex h-[80vh] overflow-x-scroll ">
+    <div className="flex mt-[10vh] gap-10 px-[45px] ">
+      {artworks.map((artwork, index) => (
+        <img
+          key={index}
+          src={require(`../assets/${artwork}`)}
+          alt={`Artwork ${index + 1}`}
+          className="h-[25vh] hover:scale-150 transition-all ease-in duration-500 rounded-[10px]"
+        />
+      ))}
+    </div>
+								</div>
+  );
 }
 
-
-
+export default Artworks;
