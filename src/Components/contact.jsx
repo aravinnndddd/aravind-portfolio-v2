08a29b9c37c46836dfc9ccbeab0f1f31d227cb8e
Bhavana <aromalaravind0@gmail.com>
2025-04-28 21:21:11 +0530

    ui

--- src/Components/contact.jsx
+++ src/Components/contact.jsx
@@ -6,16 +6,16 @@ import MoonViewer from './models'
 export const Contact = () => {
   return (
     <div id="contact" className='h-[100vh] bg-black flex items-center justify-center'>
-      <div className='h-[70vh] w-[70%] flex  bg-white/10 rounded-3xl text-white '>
+      <div className='shadow-lg shadow-cyan-500/50 ring-2 ring-purple-500 md:h-[70vh] w-[70%] md:flex  bg-white/10 rounded-3xl text-white '>
      
-        <form action="" className='flex flex-col gap-4  justify-center mx-[100px] my-[10vh]'>
+        <form action="" className='filter- flex flex-col gap-4  justify-center mx-10 md:my-[10vh] mt-[50px]'>
           <fieldset><legend>Name:</legend>
-          <input type="text" placeholder='Your name' name='name' className='w-[300px] h-[5vh] px-2 rounded bg-white/20' /></fieldset>
+          <input type="text" placeholder='Your name' name='name' className='ring-2 ring-blue-500 w-[90%] md:w-[300px] md:h-[5vh] px-2 rounded bg-white/20' /></fieldset>
           <fieldset><legend>Email:</legend>
-          <input type="text" placeholder='example@gmail.com' name='email' className='w-[300px] px-2 h-[5vh] rounded bg-white/20' /></fieldset>
+          <input type="text" placeholder='example@gmail.com' name='email' className='ring-2 ring-blue-500 w-[90%] md:w-[100%] px-2 md:h-[5vh] rounded bg-white/20' /></fieldset>
           <fieldset><legend>Enquiry:</legend>
-          <textarea name="enquiries" className='w-[100%] h-[15vh] rounded bg-white/20'></textarea></fieldset>
-          <div className='flex justify-end w-[100%]'><button className='w-[100px] px-2 h-[5vh] rounded bg-white/20'>Submit</button></div>
+          <textarea name="enquiries" className='ring-2 ring-blue-500 w-[90%] md:w-[100%] md:h-[15vh] rounded bg-white/20'></textarea></fieldset>
+          <div className='flex justify-end w-[100%]'><button className='w-[100px] px-2 mx-auto md:h-[5vh] rounded bg-white/20 ring-2 ring-blue-500 '>Submit</button></div>
         </form>
         <MoonViewer/>
       </div>
--- src/Components/models.jsx
+++ src/Components/models.jsx
@@ -1,21 +1,29 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Environment, useGLTF } from "@react-three/drei";
 
 function Model() {
   const { scene } = useGLTF("/models/moon.glb");
-  return <primitive object={scene} scale={1.5} />;
+  const modelRef = useRef();
+
+  useFrame(() => {
+    if (modelRef.current) {
+      modelRef.current.rotation.y += 0.003;
+    }
+  });
+
+  return <primitive ref={modelRef} object={scene} scale={1.5} />;
 }
 
 const MoonViewer = () => {
   return (
-    <div style={{ width: "100%", height: "500px" }}>
-      <Canvas camera={{ position: [0, 0, 5], fov: 45 }}>
+    <div className="h-[40vh] w-full md:h-[70vh] md:w-[60%] flex items-center justify-center">
+      <Canvas camera={{ position: [0, 0, 5], fov: 65 }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[5, 5, 5]} />
         <Suspense fallback={null}>
           <Model />
-          <OrbitControls />
+          <OrbitControls enableZoom={false} />
           <Environment preset="sunset" />
         </Suspense>
       </Canvas>
