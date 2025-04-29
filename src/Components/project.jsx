59c437d26c29d667041778f7b3106e0c5ef51cc1
Bhavana <aromalaravind0@gmail.com>
2025-04-29 13:02:53 +0530

    project

--- src/Components/project.jsx
+++ src/Components/project.jsx
@@ -1,22 +1,77 @@
-import React from 'react'
+import React from "react";
 
 export const Project = () => {
   return (
-    <div id="projects" className='flex flex-col h-[100vh] bg-black'>
- <h1 className='text-white text-center font-bold text-4xl pb-[10px]'>Projects</h1>
-        <div className=' flex flex-row justify-center gap-2 w-[100%] h-[40vh] bg-black'>
-     <div className='flex flex-col w-[30%] h-[35vh] bg-purple-500/20 rounded-lg'>
-     <h1 className='text-white text-center text-2xl mt-[10px]'>Project-1</h1>
+    <div id="projects" className="flex flex-col h-[100vh] bg-black">
+      <h1 className="text-white text-center font-bold text-4xl pb-[10px]">
+        Projects
+      </h1>
+     <div className="flex justify-center mt-[20vh]">
+     <div className="flex md:flex-col  w-[70%] justify-center absolute h-[55vh] shadow-lg ring-2 bg-blue-950 translate-y-[15vh] shadow-indigo-500/50 rounded-lg">
+     <div className="absolute top-[25px] left-[25px] flex gap-2 w-fit h-[30px]">
+        <span className="bg-red-500 w-4 h-4 rounded-full block"></span>
+        <span className="bg-yellow-500 w-4 h-4 rounded-full block"></span>
+        <span className="bg-green-500 w-4 h-4 rounded-full block"></span>
+      </div>
+          <h1 className="text-white text-center text-2xl mt-[10px]">
+            Project-1
+          </h1>
+        </div>
+        <div className="flex md:flex-col shadow-indigo-500/50 w-[72%] justify-center absolute h-[55vh] shadow-lg ring-2  bg-blue-950 translate-y-[12vh] rounded-lg">
+        <div className="absolute top-[25px] left-[25px] flex gap-2 w-fit h-[30px]">
+        <span className="bg-red-500 w-4 h-4 rounded-full block"></span>
+        <span className="bg-yellow-500 w-4 h-4 rounded-full block"></span>
+        <span className="bg-green-500 w-4 h-4 rounded-full block"></span>
+      </div>
+          <h1 className="text-white text-center text-2xl mt-[10px]">
+            Project-2
+          </h1>
+        </div>
+        <div className="flex md:flex-col shadow-indigo-500/50 w-[74%] justify-center absolute h-[55vh] shadow-lg ring-2  bg-blue-950 translate-y-[9vh] rounded-lg">
+        <div className="absolute top-[25px] left-[25px] flex gap-2 w-fit h-[30px]">
+        <span className="bg-red-500 w-4 h-4 rounded-full block"></span>
+        <span className="bg-yellow-500 w-4 h-4 rounded-full block"></span>
+        <span className="bg-green-500 w-4 h-4 rounded-full block"></span>
+      </div>
+          <h1 className="text-white text-center text-2xl mt-[10px]">
+            Project-3
+          </h1>
+        </div>
+     
+    
+        <div className="md:flex md:flex-col shadow-indigo-500/50 w-[76%] justify-center absolute h-[55vh] shadow-lg ring-2  bg-blue-950 translate-y-[6vh] rounded-lg">
+        <div className="absolute top-[25px] left-[25px] flex gap-2 w-fit h-[30px]">
+        <span className="bg-red-500 w-4 h-4 rounded-full block"></span>
+        <span className="bg-yellow-500 w-4 h-4 rounded-full block"></span>
+        <span className="bg-green-500 w-4 h-4 rounded-full block"></span>
+      </div>
+          <h1 className="text-white text-center text-2xl mt-[10px]">
+            Project-4
+          </h1>
+        </div>
+        <div className="flex md:flex-col shadow-indigo-500/50 w-[78%] justify-center absolute h-[55vh] ring-2 bg-blue-950 shadow-lg translate-y-[3vh] rounded-lg">
+        <div className="absolute top-[25px] left-[25px] flex gap-2 w-fit h-[30px]">
+        <span className="bg-red-500 w-4 h-4 rounded-full block"></span>
+        <span className="bg-yellow-500 w-4 h-4 rounded-full block"></span>
+        <span className="bg-green-500 w-4 h-4 rounded-full block"></span>
+      </div>
+          <h1 className="text-white text-center text-2xl mt-[10px]">
+            Project-5
+          </h1>
+        </div>
+        <div className=" flex md:flex-col shadow-indigo-500/50 w-[80%] justify-center absolute h-[55vh] ring-2 shadow-lg  bg-blue-950  rounded-lg translate-y-[.2vh]">
+        <div className="absolute top-[25px] left-[25px] flex gap-2 w-fit h-[30px]">
+        <span className="bg-red-500 w-4 h-4 rounded-full block"></span>
+        <span className="bg-yellow-500 w-4 h-4 rounded-full block"></span>
+        <span className="bg-green-500 w-4 h-4 rounded-full block"></span>
+      </div>
+          <h1 className="text-white text-center text-2xl  md:mt-[20px] mt-[50px]">
+            Project-1
+          </h1>
+        </div>
      </div>
-     <div className='flex flex-col w-[30%] h-[35vh] bg-purple-500/20 rounded-lg'><h1 className='text-white text-center text-2xl mt-[10px]'>Project-2</h1></div>
-     <div className='flex flex-col w-[30%] h-[35vh] bg-purple-500/20 rounded-lg'><h1 className='text-white text-center text-2xl mt-[10px]'>Project-3</h1></div>
-     </div>
-     <div className=' flex flex-row justify-center gap-2 w-[100%] h-[40vh] bg-black'>
-     <div className='flex flex-col w-[30%] h-[35vh] bg-purple-500/20 rounded-lg'><h1 className='text-white text-center text-2xl mt-[10px]'>Project-4</h1></div>
-     <div className='flex flex-col w-[30%] h-[35vh] bg-purple-500/20 rounded-lg'><h1 className='text-white text-center text-2xl mt-[10px]'>Project-5</h1></div>
-     <div className='flex flex-col w-[30%] h-[35vh] bg-purple-500/20 rounded-lg'><h1 className='text-white text-center text-2xl mt-[10px]'>Project-6</h1></div>
-    </div>
-  
-     </div>
-  )
-}
+      
+      </div>
+    
+  );
+};
