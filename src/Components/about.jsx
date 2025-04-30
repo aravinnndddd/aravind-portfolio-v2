8b2306126232fa85dca3cbf55ecaf72f14f30faf
Bhavana <aromalaravind0@gmail.com>
2025-04-30 15:01:46 +0530

    added projects

--- src/Components/about.jsx
+++ src/Components/about.jsx
@@ -1,99 +1,89 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
 import heropic from "/assets/bgImg1.png";
-import "../App.css"
+import "../App.css";
 import { Link } from "react-scroll";
 export default function About() {
   return (
-    <div id="about" className="h-[100vh] bg-black flex flex-col justify-center md:flex-row w-full md:pt-[10vh] px-[10%]">
-     
-
-
-    {/* Main content */}
-    <div className="h-[70vh]  w-10/12  bg-white/10 backdrop-blur-md rounded-[20px] absolute  mx-auto">
-      
-      {/* Top-left dots */}
-      <div className="relative top-[25px] left-[25px] flex gap-2 w-fit h-[30px]">
-        <span className="bg-red-500 w-4 h-4 rounded-full block"></span>
-        <span className="bg-yellow-500 w-4 h-4 rounded-full block"></span>
-        <span className="bg-green-500 w-4 h-4 rounded-full block"></span>
-      </div>
-  
-      {/* Flex container for Image + Text */}
-      <div className="flex flex-col  md:flex-row w-full px-5 h-full">
-        
-        {/* Image Section */}
-        <div className="md:w-5/12 w-8/12 mx-auto h-[30vh] md:h-[50vh] flex flex-col justify-center items-center">
-          <img id="heropic" className="md:w-full  md:h-full md:object-contain " src={heropic} alt="astro" />
+    <div
+      id="about"
+      className="h-[100vh] bg-black flex flex-col justify-center md:flex-row w-full md:pt-[10vh] px-[10%]"
+    >
+      <div className="h-[70vh]  w-10/12  bg-white/10 backdrop-blur-md rounded-[20px] absolute  mx-auto">
+        <div className="relative top-[25px] left-[25px] flex gap-2 w-fit h-[30px]">
+          <span className="bg-red-500 w-4 h-4 rounded-full block"></span>
+          <span className="bg-yellow-500 w-4 h-4 rounded-full block"></span>
+          <span className="bg-green-500 w-4 h-4 rounded-full block"></span>
         </div>
-  
-        {/* Text Section */}
-        <div className="text-purple-400 md:w-6/12 h-[50vh] flex flex-col md:justify-center leading-7 ">
-          <h2 className="font-bold md:text-3xl mb-2">Hi, I'm Aravind</h2>
-  
-          <h3 className="text-indigo-400 md:text-xl mb-4">
-            I'm a{" "}
-            <TypeAnimation
-              sequence={[
-                "Coder", 1500,
-                "Web Developer", 1500,
-                "UI Designer", 1500,
-                "Artist", 1500,
-                "Tech Enthusiast", 1500,
-                "Gamer", 1500,
-                "Student", 1500
-              ]}
-              wrapper="span"
-              speed={50}
-              repeat={Infinity}
+
+        <div className="flex flex-col  md:flex-row w-full px-5 h-full">
+          <div className="md:w-5/12 w-8/12 mx-auto h-[30vh] md:h-[50vh] flex flex-col justify-center items-center">
+            <img
+              id="heropic"
+              className="md:w-full  md:h-full md:object-contain "
+              src={heropic}
+              alt="astro"
             />
-          </h3>
-  
-          <p className="leading-7 text-base">
-            I'm a passionate first-year B.Tech student with a love for coding,{" "}
-            <Link to="artworks" smooth={true} duration={500}>
-                        
-                          <a href="#artworks" className="underline font-bold">
-                            Drawing,
-                          </a>
-                      
-                        </Link> {" "} gaming, and learing new {" "}
-            
-            <Link to="skills" smooth={true} duration={500}>
-                        
-                        <a href="#skills" className="underline font-bold">
-                        Skills
-                        </a>
-                    
-                      </Link>
-             
-          
-            . I enjoy building{" "}
+          </div>
 
-            <Link to="projects" smooth={true} duration={500}>
-                        
-                          <a href="#projects" className="underline font-bold">
-                          Projects
-                          </a>
-                      
-                        </Link>
+          <div className="text-purple-400 md:w-6/12 h-[50vh] flex flex-col md:justify-center leading-7 ">
+            <h2 className="font-bold md:text-3xl mb-2">Hi, I'm Aravind</h2>
 
-            , and I'm currently diving deep into web development. Let's{" "}
-            <Link to="contact" smooth={true} duration={500}>
-                        
-                          <a href="#contact" className="underline font-bold">
-                          Connect
-                          </a>
-                      
-                        </Link>{" "}
-            and create something amazing!
-          </p>
+            <h3 className="text-indigo-400 md:text-xl mb-4">
+              I'm {" "}
+              <TypeAnimation
+                sequence={[
+                  "a Coder",
+                  1500,
+                  "a Web Developer",
+                  1500,
+                  "a UI Designer",
+                  1500,
+                  "an Artist",
+                  1500,
+                  "a Tech Enthusiast",
+                  1500,
+                  "a Gamer",
+                  1500,
+                  "a Student",
+                  1500,
+                ]}
+                wrapper="span"
+                speed={50}
+                repeat={Infinity}
+              />
+            </h3>
+
+            <p className="leading-7 text-base">
+              I'm a passionate first-year B.Tech student with a love for coding,{" "}
+              <Link to="artworks" smooth={true} duration={500}>
+                <a href="#artworks" className="underline font-bold">
+                  Drawing,
+                </a>
+              </Link>{" "}
+              gaming, and learing new{" "}
+              <Link to="skills" smooth={true} duration={500}>
+                <a href="#skills" className="underline font-bold">
+                  Skills
+                </a>
+              </Link>
+              . I enjoy building{" "}
+              <Link to="projects" smooth={true} duration={500}>
+                <a href="#projects" className="underline font-bold">
+                  Projects
+                </a>
+              </Link>
+              , and I'm currently diving deep into web development. Let's{" "}
+              <Link to="contact" smooth={true} duration={500}>
+                <a href="#contact" className="underline font-bold">
+                  Connect
+                </a>
+              </Link>{" "}
+              and create something amazing!
+            </p>
+          </div>
         </div>
-  
-      </div> {/* end of flex row */}
-      
+      </div>
     </div>
-  </div>
-  
   );
 }
--- src/Components/project.jsx
+++ src/Components/project.jsx
@@ -1,77 +1,89 @@
-import React from "react";
+import { img } from "framer-motion/client";
+import { Link2Icon, LinkIcon } from "lucide-react";
+import React, { useState } from "react";
+import { Link } from "react-scroll";
 
 export const Project = () => {
+  const projects = [
+    { id: 6, title: "Advyka'25",img: "/assets/proj6.png",dis:"I contributed to the frontend development of the Advyka 2025 techfest website, focusing on creating a user-friendly, responsive interface. The site provided event details, schedules, and speaker info, enhancing the overall attendee experience. This project strengthened my skills in web development and UX/UI design.",link:"https://advyka.in/"  }
+   ,
+    { id: 2, title: "Popzzmedia",img: "/assets/proj2.png",dis:"PopzzMedia is a premium landing page for an online course, crafted to drive engagement and conversions. With a bold design, a persuasive headline, a demo video, and a countdown timer, it delivers a high-energy user experience optimized for action.",link:"https://popzzmedia.com/"  }
+    ,
+    { id: 1, title: "CineBox",img: "/assets/proj1.png",dis:"CineBox is an app that lets users search for movies or TV series by name. Once you search for a movie or series, the app provides a list of results based on your query. Whether you're looking for a specific title or just browsing, CineBox makes it easy to discover and explore a wide range of movies and shows.",link:"https://movieland-chi-indol.vercel.app/" },
+    { id: 3, title: "Tic-Tac-Toe",img: "/assets/proj3.png",dis:"This is a clean, offline two-player Tic Tac Toe game built for pure, old-school fun — no internet, no distractions, just you and your opponent battling it out in a classic game of strategy! The UI is simple yet stylish, making it easy for both players to jump in, mark their moves (X or O), and race to get three in a row — vertically, horizontally, or diagonally.",link:"https://tictactoe-one-flax.vercel.app/"  },
+    { id: 4, title: "Todo App",img: "/assets/proj4.png",dis:"This is a clean and minimalistic To-Do app designed for pure productivity without the clutter.With a simple, intuitive UI, you can easily add, view, and manage your daily tasks — no complicated features, no overwhelming screens. Just the essentials, right where you need them.",link:"hhttps://todo-murex-iota.vercel.app/"  },
+    { id: 5, title: "Calculator",img: "/assets/proj5.png",dis:"A lightweight calculator offering essential arithmetic operations with clean Light and Dark themes for optimal user comfort.",link:"https://aravinnndddd.github.io/calculator/"  }
+    ,
+  ];
+
+  const translateYValues = ["7vh", "5.8vh", "4.5vh", "3vh", "1.5vh", ".2vh"];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNextProject = () => {
+    setCurrentIndex((prev) => (prev + 1) % projects.length);
+  };
+  const handleprevProject = () => {
+    setCurrentIndex((prev) => (prev - 1 + projects.length) % projects.length);
+  };
   return (
     <div id="projects" className="flex flex-col h-[100vh] bg-black mt-[5vh]">
       <h1 className="text-white text-center font-bold text-4xl pb-[10px] mt-[10vh]">
         Projects
       </h1>
-     <div className="flex justify-center ">
-     <div className="flex md:flex-col  w-[70%] justify-center absolute h-[40vh] md:h-[55vh] shadow-md ring-2 bg-blue-950 md:translate-y-[15vh] translate-y-[7vh] shadow-purple-500/50 ring-purple-600 rounded-lg">
-     <div className="absolute top-[25px] left-[25px] flex gap-2 w-fit h-[30px]">
-        <span className="bg-red-500 w-4 h-4 rounded-full block"></span>
-        <span className="bg-yellow-500 w-4 h-4 rounded-full block"></span>
-        <span className="bg-green-500 w-4 h-4 rounded-full block"></span>
+
+      <div className="flex justify-center ">
+        {projects.map((project, index) => {
+          // Calculate stacking order
+          const zIndex = projects.length - index;
+          // Check if this is the current active card
+          const isActive = index === currentIndex;
+
+          return (
+            <div
+             
+              key={project.id}
+              className={`flex md:flex-col w-[95%]  justify-center absolute h-[60vh] md:h-[55vh] md:w-[70%]
+                shadow-md ring-2 bg-blue-950 shadow-purple-500/50 ring-purple-600 rounded-lg
+                transition-all duration-500 ease-in-out`}
+              style={{
+                transform: `translateY(${translateYValues[index] || "0vh"}) ${
+                  isActive ? "scale(1)" : "scale(0.95)"
+                }`,
+                zIndex: isActive ? 50 : zIndex,
+                opacity: isActive ? 1 : 0.7,
+              }}
+            >
+              
+
+              {/* Project Title */}
+              <h1 className="text-white md:relative md:top-[25px] absolute text-center text-2xl top-[40px] font-extrabold ">
+                {project.title}
+              </h1>
+                <div className="flex md:flex-row flex-col  items-center h-full gap-5 mx-5">
+               
+                <img  className="rounded-lg md:w-[40%] flex md:flex-col md:h-[40vh] mt-[10vh] md:mt-[10px]" src={project.img} alt="proj"/>
+                <div className="flex flex-col  md:w-[60%] md:text-xl text-[12px] w-[100%] font-semibold text-white">
+                  {project.dis}
+                 <br />
+                 <a className="hover:text-white focus:hover:text-white transition-all ease text-neutral-500 flex underline font-normal text-[13px]" href={project.link}>Click here <LinkIcon width={15}/> </a>
+                </div>
+               
+                </div>
+               {/* Colored Dots */}
+              <div  className="absolute md:top-[25px] md:left-[25px] top-[15px] left-[15px] flex gap-2 w-fit h-[30px]">
+                <div
+                  onClick={handleNextProject}
+                  className="bg-red-500 md:w-8 md:h-8 h-6 w-6 rounded-full block cursor-pointer"
+                ></div>
+                <span  className="bg-yellow-500 md:w-8 md:h-8 h-6 w-6 rounded-full block"></span>
+                <div onClick={handleprevProject} className="bg-green-500 md:w-8 md:h-8 h-6 w-6 flex cursor-pointer rounded-full"></div>
+              </div>
+              </div>
+             
+            
+          );
+        })}
       </div>
-          <h1 className="text-white text-center text-2xl mt-[10px]">
-            Project-1
-          </h1>
-        </div>
-        <div className="flex md:flex-col shadow-purple-500/50 ring-purple-600 w-[72%] justify-center absolute h-[40vh] md:h-[55vh] shadow-md ring-2  bg-blue-950 md:translate-y-[12vh] translate-y-[5.8vh] rounded-lg">
-        <div className="absolute top-[25px] left-[25px] flex gap-2 w-fit h-[30px]">
-        <span className="bg-red-500 w-4 h-4 rounded-full block"></span>
-        <span className="bg-yellow-500 w-4 h-4 rounded-full block"></span>
-        <span className="bg-green-500 w-4 h-4 rounded-full block"></span>
-      </div>
-          <h1 className="text-white text-center text-2xl mt-[10px]">
-            Project-2
-          </h1>
-        </div>
-        <div className="flex md:flex-col shadow-purple-500/50 ring-purple-600 w-[74%] justify-center absolute h-[40vh] md:h-[55vh] shadow-md ring-2  bg-blue-950 md:translate-y-[9vh] translate-y-[4.5vh] rounded-lg">
-        <div className="absolute top-[25px] left-[25px] flex gap-2 w-fit h-[30px]">
-        <span className="bg-red-500 w-4 h-4 rounded-full block"></span>
-        <span className="bg-yellow-500 w-4 h-4 rounded-full block"></span>
-        <span className="bg-green-500 w-4 h-4 rounded-full block"></span>
-      </div>
-          <h1 className="text-white text-center text-2xl mt-[10px]">
-            Project-3
-          </h1>
-        </div>
-     
-    
-        <div className="md:flex md:flex-col shadow-purple-500/50 ring-purple-600 w-[76%] justify-center absolute h-[40vh] md:h-[55vh] shadow-md ring-2  bg-blue-950 md:translate-y-[6vh] translate-y-[3vh] rounded-lg">
-        <div className="absolute top-[25px] left-[25px] flex gap-2 w-fit h-[30px]">
-        <span className="bg-red-500 w-4 h-4 rounded-full block"></span>
-        <span className="bg-yellow-500 w-4 h-4 rounded-full block"></span>
-        <span className="bg-green-500 w-4 h-4 rounded-full block"></span>
-      </div>
-          <h1 className="text-white text-center text-2xl mt-[10px]">
-            Project-4
-          </h1>
-        </div>
-        <div className="flex md:flex-col shadow-purple-500/50 ring-purple-600 w-[78%] justify-center absolute h-[40vh] md:h-[55vh] ring-2 bg-blue-950 shadow-md md:translate-y-[3vh] translate-y-[1.5vh] rounded-lg">
-        <div className="absolute top-[25px] left-[25px] flex gap-2 w-fit h-[30px]">
-        <span className="bg-red-500 w-4 h-4 rounded-full block"></span>
-        <span className="bg-yellow-500 w-4 h-4 rounded-full block"></span>
-        <span className="bg-green-500 w-4 h-4 rounded-full block"></span>
-      </div>
-          <h1 className="text-white text-center text-2xl mt-[10px]">
-            Project-5
-          </h1>
-        </div>
-        <div className=" flex md:flex-col shadow-purple-500/50 w-[80%] justify-center absolute h-[40vh] md:h-[55vh] ring-2 ring-purple-600 shadow-md  bg-blue-950  rounded-lg translate-y-[.2vh]">
-        <div className="absolute top-[25px] left-[25px] flex gap-2 w-fit h-[30px]">
-        <span className="bg-red-500 w-4 h-4 rounded-full block"></span>
-        <span className="bg-yellow-500 w-4 h-4 rounded-full block"></span>
-        <span className="bg-green-500 w-4 h-4 rounded-full block"></span>
-      </div>
-          <h1 className="text-white text-center text-2xl  md:mt-[20px] mt-[10vh]">
-            Project-1
-          </h1>
-        </div>
-     </div>
-      
-      </div>
-    
+    </div>
   );
 };
