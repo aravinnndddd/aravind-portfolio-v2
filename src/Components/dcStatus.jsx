cba62752f52b49a673da642d6eebe1f9f8c0ba06
aravinnndddd <aromalaravind0@gmail.com>
2025-07-04 17:41:33 +0530

    Update dcStatus.jsx

--- src/Components/dcStatus.jsx
+++ src/Components/dcStatus.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const DcStatus = () => {
   return (
-   <iframe src="https://aravind-dc.vercel.app/"  height="100%" frameborder="0" className=' rounded-lg'></iframe>
+   <iframe src="https://status-dc.vercel.app/"  height="100%" frameborder="0" className=' rounded-lg'></iframe>
   )
 }
