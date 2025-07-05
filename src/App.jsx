29781527f6a6dd60c3d1520ff9a001c73b267643
Aravind <aromalaravind0@gmail.com>
2025-07-06 00:10:38 +0530

    feat(image slider):added image slider component

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,41 @@
-
 // import reactLogo from './assets/react.svg'
 
-import './App.css'
-import About from './Components/about'
-import Artworks from './Components/artworks'
-import Navbar from './Components/navbar'
-import {Element } from 'react-scroll';
-import Skills from './Components/skills';
-import { Project } from './Components/project';
-import { Contact } from './Components/contact';
-import { Footer } from './Components/footer';
+import "./App.css";
+import About from "./Components/about";
+import { Artworks } from "./Components/artworks";
+import Navbar from "./Components/navbar";
+import { Element } from "react-scroll";
+import Skills from "./Components/skills";
+import { Project } from "./Components/project";
+import { Contact } from "./Components/contact";
+import { Footer } from "./Components/footer";
 
 function App() {
-
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <Element name="aravind">
-          <About/>
+        <About />
       </Element>
-   
-      
+
       <Element name="skills">
         <Skills />
       </Element>
       <Element name="artworks">
-        <Artworks/>
+        <Artworks />
       </Element>
       <Element name="projects">
         <Project />
       </Element>
       <Element name="contact">
         <Contact />
-        
       </Element>
-      
+
       <Element name="footer">
         <Footer />
       </Element>
-     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/artworks.jsx
+++ src/Components/artworks.jsx
@@ -1,30 +1,43 @@
-
-const artworks= [
-  'work2.png',
-  'work3.png',
-  'work4.png',
-  'work5.png',
-  'work6.png',
-  'work7.png',
-  'work8.png',
-  'work1.png',
-  'work10.jpg','work11.jpeg','work13.jpeg','work14.jpeg',
-];
-
-export default function Artworks() {
+import work1 from "/assets/work2.png";
+import work2 from "/assets/work3.png";
+import work3 from "/assets/work4.png";
+import work4 from "/assets/work5.png";
+import work5 from "/assets/work6.png";
+import work6 from "/assets/work7.png";
+import work7 from "/assets/work8.png";
+import work8 from "/assets/work1.png";
+import work9 from "/assets/work10.jpg";
+import work10 from "/assets/work11.jpeg";
+import work11 from "/assets/work13.jpeg";
+import work12 from "/assets/work14.jpeg";
+import Marquee from "react-fast-marquee";
+import { div } from "framer-motion/client";
+export const Artworks = () => {
   return (
-							<div id="artworks" className="border-b-2 border-t-2 border-neutral-500 flex h-[55vh]  m-5 items-center overflow-x-scroll ">
-    <div className="flex gap-10 px-[45px] ">
-      {artworks.map((artwork, index) => (
-        <img
-          key={index}
-          src={`/assets/${artwork}`}
-          alt={`Artwork ${index + 1}`}
-          className="h-[25vh] hover:scale-150 transition-all ease duration-300 rounded-[10px]"
-        />
+    <Marquee pauseOnHover speed={30}>
+      {[
+        work1,
+        work2,
+        work3,
+        work4,
+        work5,
+        work6,
+        work7,
+        work8,
+        work9,
+        work10,
+        work11,
+        work12,
+      ].map((logo, index) => (
+        <div className="h-[45vh] border-b-2 border-t-2 border-neutral-500 flex items-center">
+          <img
+            key={index}
+            src={logo}
+            alt="logo"
+            className="h-[25vh] m-5  hover:scale-150 transition-all ease duration-300 rounded-[10px]"
+          />
+        </div>
       ))}
-    </div>
-								</div>
+    </Marquee>
   );
-}
-
+};
