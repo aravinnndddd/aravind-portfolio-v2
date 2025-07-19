5754d03d16e1ede232be68e0839b98cc1d13957a
Aravind <aromalaravind0@gmail.com>
2025-07-19 13:02:26 +0530

    seo vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import pagesSitemap from 'vite-plugin-pages-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    pagesSitemap({
+      hostname: 'https://aravind-p.vercel.app'
+    })
+  ],
 })
